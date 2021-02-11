2f1a680b4680b76c9991cd61b52e585c33991122
Brenner Campos <brennercampos@gmail.com>
2021-02-10 19:31:36 -0500

    class #2 v1.3

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 //in other functions that this function calls. don't read from sensors or
 //use actuators frmo main or you will get a panic.
 //add
-func robotRunLoop(lightSensor *aio.GroveLightSensorDriver, soundSensor *aio.GroveSoundSensorDriver, gpg *g.Driver) {
+func robotRunLoop(lightSensor *aio.GroveLightSensorDriver, lightSensor2 *aio.GroveLightSensorDriver, gpg *g.Driver) {
 	ledOn := true
 
 	// hey this is a test ~~~
@@ -26,12 +26,14 @@ func robotRunLoop(lightSensor *aio.GroveLightSensorDriver, soundSensor *aio.Grov
 		if err != nil {
 			fmt.Errorf("Error reading light sensor %+v", err)
 		}
-		soundSensorVal, err := soundSensor.Read()
+		//soundSensorVal, err := soundSensor.Read()
+		sensorVal2, err := lightSensor.Read()
 		if err != nil {
 			fmt.Errorf("Error reading from Sound Sensor %+v", err)
 		}
-		fmt.Println("Light Value is ", sensorVal)
-		fmt.Println("Sound Value is ", soundSensorVal)
+		fmt.Println("Light (AD_2_1 Value is ", sensorVal)
+		fmt.Println("Light (AD_1_1 Value is ", sensorVal2)
+		fmt.Println("Sound Value is ", sensorVal2)
 		time.Sleep(time.Second)
 		gpg.SetLED(1, 200, 200, 200)
 
@@ -74,7 +76,8 @@ func main() {
 	raspiAdaptor := raspi.NewAdaptor()
 	gopigo3 := g.NewDriver(raspiAdaptor)
 	lightSensor := aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1") //AnalogDigital Port 1 is "AD_1_1" this is port 2
-	soundSensor := aio.NewGroveSoundSensorDriver(gopigo3, "AD_1_1")
+	lightSensor2 := aio.NewGroveLightSensorDriver(gopigo3, "AD_1_1")
+	//soundSensor := aio.NewGroveSoundSensorDriver(gopigo3, "AD_1_1")
 	//end create hardware drivers
 
 	//here we create an anonymous function assigned to a local variable
@@ -82,14 +85,15 @@ func main() {
 	//I'm calling my robot main loop here. Pass any of the variables we created
 	//above to that function if you need them
 	mainRobotFunc := func() {
-		robotRunLoop(lightSensor, soundSensor, gopigo3)
+		//robotRunLoop(lightSensor, soundSensor, gopigo3)
+		robotRunLoop(lightSensor, lightSensor2, gopigo3)
 	}
 
 	//this is the crux of the gobot framework. The factory function to create a new robot
 	//struct (go uses structs and not objects) It takes four parameters
 	robot := gobot.NewRobot("gopigo3sensorChecker", //first a name
-		[]gobot.Connection{raspiAdaptor},                  //next a slice of connections to one or more robot controllers
-		[]gobot.Device{gopigo3, lightSensor, soundSensor}, //next a slice of one or more sensors and actuators for the robots
+		[]gobot.Connection{raspiAdaptor},                   //next a slice of connections to one or more robot controllers
+		[]gobot.Device{gopigo3, lightSensor, lightSensor2}, //next a slice of one or more sensors and actuators for the robots
 		mainRobotFunc, //the variable holding the function to run in a new thread as the main function
 	)
 
